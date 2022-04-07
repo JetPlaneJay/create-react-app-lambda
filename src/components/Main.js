80929e8b4063ba4edd560b3e737c9ebeba29c6ac
Jacob Anthony Tabares <73261191+JetPlaneJay@users.noreply.github.com>
2022-04-06 18:53:02 -0700

    Create Main.js

---
+++ src/components/Main.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+
+export default function Main() {
+    return (
+        <main>
+            <article>
+                <h2>About</h2>
+                <p>I am a frontend developer with a 
+                    particular interest in making things 
+                    simple. Current personal project is a volunteer management system.</p>
+            </article>
+            <article>
+                <h2>Interests</h2>
+                <p>Running. StarCraft. Building/Repairing/Cleaning my PC. Travel</p>
+            </article>
+           
+        </main>
+    )
+}
